0f1d2f2b4693f9350a873b62d40bd7b5724891dd
Ailton Alves da Silva <ailton2211@gmail.com>
2023-06-29 21:31:02 -0300

    Alterações iniciais e testes.

---
+++ MvcLivros/Controllers/OlaMundoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMovie.Controllers;
+
+public class OlaMundoController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult BemVindo(string nome = "Ailton", int numTimes = 1)
+    {
+        ViewData["Message"] = "Olá " + nome;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
